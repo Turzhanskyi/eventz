1a79afd4db68a36b4095a388226ebbd40561a93e
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2020-12-07 00:07:19 +0200

    Add One-to-Many: Logic

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module RegistrationsHelper
+  def register_or_sold_out(event)
+    if event.sold_out?
+      content_tag(:span, 'Sold Out!', class: 'sold-out')
+    else
+      link_to 'Register!', new_event_registration_path(event),
+              class: 'register'
+    end
+  end
 end
